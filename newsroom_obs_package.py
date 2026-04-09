372f1b6ef1dd6673b67ec03510019953d92d56e8
embarkaselka-cmyk <embarkaselka@gmail.com>
2026-04-09 18:22:46 +0100

    Fix scene release type in OBS Python script

--- newsroom_obs_package.py
+++ newsroom_obs_package.py
@@ -114,9 +114,9 @@ def on_next_headline_button(props, prop):
 def ensure_scene(scene_name):
     source = obs.obs_get_source_by_name(scene_name)
     if source is None:
-        scene_source = obs.obs_scene_create(scene_name)
-        if scene_source is not None:
-            obs.obs_source_release(scene_source)
+        scene = obs.obs_scene_create(scene_name)
+        if scene is not None:
+            obs.obs_scene_release(scene)
     else:
         obs.obs_source_release(source)
 
